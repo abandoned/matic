42fac99ea1f66cb21ec630c1843d3ae4b8a4e0d8
Hakan Ensari <hakan.ensari@papercavalier.com>
2010-11-12 20:37:13 +0000

    Remove collections before each spec

--- spec/support/mongomatic.rb
+++ spec/support/mongomatic.rb
@@ -1,3 +1,9 @@
 require "mongomatic"
 
 Mongomatic.db = Mongo::Connection.new.db("matic_test")
+
+Rspec.configure do |config|
+  config.before do
+    Mongomatic.db.collections.each { |c| c.remove }
+  end
+end
